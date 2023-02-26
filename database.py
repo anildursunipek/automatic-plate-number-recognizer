e0060d90a737ba8c343d0f859448bfe2751c3e75
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-02-26 18:33:38 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,29 @@
 from pymongo import MongoClient
+from abc import ABC, abstractmethod
 
-class SaveMongoDb:
+class PlateRepository(ABC):
+
+    @abstractmethod
+    def save(self, plateNumber:str):
+        pass
+
+class MongoDB(PlateRepository):
     def __init__(self, connectionString: str):
+        # Connect to database
         self._connectionString = connectionString
         self._client = MongoClient(self._connectionString)
         self._database = self._client["plate-db"]
         self._collection = self._database["plate"]
 
-    def save_to_database(self, text):
+    def save(self, plateNumber):
         document = {
-            "plateNumber": text
+            "plateNumber": plateNumber
         }
         self._collection.insert_one(document)
 
 if __name__ == "__main__":
     # Test Code
-    test = SaveMongoDb("mongodb://localhost:27017")
+    test = MongoDB("mongodb://localhost:27017")
     test.save_to_database("15ABS875")
 
 
