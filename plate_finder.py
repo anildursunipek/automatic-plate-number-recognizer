4bebeb78c244796b6c858847d5706397e89458e8
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-02-26 19:20:44 +0300

    Update plate_finder.py

--- plate_finder.py
+++ plate_finder.py
@@ -3,13 +3,44 @@ import torch
 import cv2
 import numpy as np
 from datetime import datetime
-import keras_ocr
 from database import MongoDB
+from plate_reader import PlateReader
 
 
 class PlateFinder(threading.Thread):  
   """
-  This class
+  This class finds the license plate from the frame and saves the license plate.
+  Yolov5 model use for plate detection.
+  Keras ocr use while determining the number on the plate.
+
+  ...
+
+  Attributes
+  ----------
+  theadName: str
+    name of the main thread
+  video_source: int(0, 1, ...)
+    source camera to be used for detection
+  video_out: str
+    output path for the video writer. If it is None it will not record.
+  model: 
+    plate detection model
+  model.conf: between 0.00 to 1.00
+    The confidence determines how certain the model is that the prediction received matches to a certain class.
+  model.iou: between 0.00 to 1.00
+    IoU is a measure of overlap between two bounding boxes
+  plateReader: PlateReader
+    plate reader object
+  
+  Methods
+  -------
+  run():
+  load_model(yolov5Path, customWeightsPath):
+  detection(frame):
+  start_detection():
+  save_plate(frame, labels, cordinates, take_photo):
+  take_snapshot():
+  save(text):
   """
   def __init__(self, threadName: str, video_source, video_out = None):
     threading.Thread.__init__(self) 
@@ -18,14 +49,15 @@ class PlateFinder(threading.Thread):
     self.video_out = video_out
     print("[INFO] Loading Model. . .")
     self.model = self.load_model(yolov5Path= "yolov5", customWeightsPath= "yolov5/best.pt")
-    self.model.conf = 0.50 # NMS confidence threshold
-    self.model.iou = 0.50 # NMS IoU threshold
+    self.model.conf = 0.45 # NMS confidence threshold
+    self.model.iou = 0.45 # NMS IoU threshold
     threading.Timer(10.0, self.take_snapshot).start()
-    self.pipeline = keras_ocr.pipeline.Pipeline()
+    self.plateReader = PlateReader()
     self.mongodb = MongoDB("mongodb://localhost:27017")
     
   def run(self):
     """
+    Method used to start threads
     """
     self.start_detection()
 
@@ -41,7 +73,35 @@ class PlateFinder(threading.Thread):
       A pytorch model which is loading custom weights.
     """
     return torch.hub.load(repo_or_dir= yolov5Path, model= 'custom', source= 'local', path= customWeightsPath, force_reload= True)
+  
+  def detection(self, frame):
+    """ 
+    Function description
+    ...
+    Parameters:
+      result:
+      labels:
+      cordinates:
 
+    Returns:
+      result:
+      labels:
+      cordinates:
+
+    Example
+    -------
+    print(result.xyxyn[0])
+    
+    #      xmin    ymin    xmax   ymax  confidence  class    name
+    # 0  749.50   43.50  1148.0  704.5    0.874023      0  person
+    # 1  433.50  433.50   517.5  714.5    0.687988     27     tie
+    # 2  114.75  195.75  1095.0  708.0    0.624512      0  person
+    # 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
+    """
+    result = self.model(frame)
+    labels, cordinates = result.xyxy[0][:,-1], result.xyxy[0][:,:-2]
+    return result, labels, cordinates
+  
   def start_detection(self):
     """
     Function description
@@ -118,19 +178,8 @@ class PlateFinder(threading.Thread):
           obj_path = f'''./detected/{obj_filename}.png'''
           #cv2.imwrite(f'''{obj_path}''', plateFrame)
           print("Number of active threads:", threading.active_count())
-          text = self.text_detection(plateFrame)
+          text = self.plateReader.read(plateFrame)
           self.save(text)
-
-  def text_detection(self, plateFrame):
-    result = self.pipeline.recognize([plateFrame])
-    temp_arr = []
-    for idx in result[0]:
-      temp_arr.append(idx[1][0][0])
-      text = ""
-    for idx in range(len(temp_arr)):
-      text += f"{result[0][temp_arr.index(np.min(temp_arr))][0]}"
-      temp_arr[temp_arr.index(np.min(temp_arr))] += 50000
-    return text
 
   def take_snapshot(self):
     """
@@ -140,43 +189,15 @@ class PlateFinder(threading.Thread):
     """
     print("Take snapshot init")
     self.save_plate(frame, labels, cordinates, True)
-    thread = threading.Timer(5.0, self.take_snapshot)
+    thread = threading.Timer(4.0, self.take_snapshot)
     thread.daemon = True
     thread.start()
 
   def save(self, text):
-    #print(text)
+    print(text)
     thread = threading.Thread(target=self.mongodb.save, args=(text,))
     thread.daemon = True
     thread.start()
-
-  def detection(self, frame, model):
-    """ 
-    Function description
-    ...
-    Parameters:
-      result:
-      labels:
-      cordinates:
-
-    Returns:
-      result:
-      labels:
-      cordinates:
-
-    Example
-    -------
-    print(result.xyxyn[0])
-    
-    #      xmin    ymin    xmax   ymax  confidence  class    name
-    # 0  749.50   43.50  1148.0  704.5    0.874023      0  person
-    # 1  433.50  433.50   517.5  714.5    0.687988     27     tie
-    # 2  114.75  195.75  1095.0  708.0    0.624512      0  person
-    # 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
-    """
-    result = self.model(frame)
-    labels, cordinates = result.xyxy[0][:,-1], result.xyxy[0][:,:-2]
-    return result, labels, cordinates
 
 if __name__ == '__main__':
   # Test Code
