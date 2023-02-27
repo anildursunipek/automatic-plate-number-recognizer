256bafad101e3c0de1b5b882417158d116365d00
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-02-27 15:05:00 +0300

    Update plate_reader.py

--- plate_reader.py
+++ plate_reader.py
@@ -1,11 +1,13 @@
 import keras_ocr
 import numpy as np
+import pytesseract
 
 class PlateReader:
     def __init__(self):
         self.pipeline = keras_ocr.pipeline.Pipeline()
+        pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-    def read(self, plateFrame):
+    def read_keras_ocr(self, plateFrame ,text):
         result = self.pipeline.recognize([plateFrame])
         temp_arr = []
         for idx in result[0]:
@@ -16,3 +18,6 @@ class PlateReader:
             temp_arr[temp_arr.index(np.min(temp_arr))] += 50000
         return text
 
+    def read_tesseract(self, plateFrame, text):
+        text = pytesseract.image_to_string(plateFrame, lang ='eng', config = '-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 6 --oem 3')
+        return text
