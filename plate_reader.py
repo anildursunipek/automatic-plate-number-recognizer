bf5a67e0ed6f475786338313914775a66f9abf26
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-02-28 09:37:17 +0300

    Update plate_reader.py

--- plate_reader.py
+++ plate_reader.py
@@ -8,16 +8,22 @@ class PlateReader:
         pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
     def read_keras_ocr(self, plateFrame ,text):
-        result = self.pipeline.recognize([plateFrame])
-        temp_arr = []
-        for idx in result[0]:
-            temp_arr.append(idx[1][0][0])
-            text = ""
-        for idx in range(len(temp_arr)):
-            text += f"{result[0][temp_arr.index(np.min(temp_arr))][0]}"
-            temp_arr[temp_arr.index(np.min(temp_arr))] += 50000
-        return text
+        try:
+            result = self.pipeline.recognize([plateFrame])
+            temp_arr = []
+            for idx in result[0]:
+                temp_arr.append(idx[1][0][0])
+                text = ""
+            for idx in range(len(temp_arr)):
+                text += f"{result[0][temp_arr.index(np.min(temp_arr))][0]}"
+                temp_arr[temp_arr.index(np.min(temp_arr))] += 50000
+            return text
+        except:
+            return ""
 
     def read_tesseract(self, plateFrame, text):
-        text = pytesseract.image_to_string(plateFrame, lang ='eng', config = '-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 6 --oem 3')
-        return text
+        try:
+            text = pytesseract.image_to_string(plateFrame, lang ='eng', config = '-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 6 --oem 3')
+            return text
+        except:
+            text= ""
